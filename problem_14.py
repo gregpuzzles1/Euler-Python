f9d7b794a1746b2d60bf59134e0a5409c0ca0a2a
Greg Christian <glchristian@comcast.net>
2012-06-18 16:31:09 -0600

    modified problem 14

--- problem_14.py
+++ problem_14.py
@@ -1,24 +1,42 @@
-largest = 0
-for n in range(2, 1000001):
-    counter = 1
-    number = n
-    while n > 1:
-        if n%2 == 0:
-            n = n/2
+# Python version = 2.7.2
+# Platform = win32
+
+import time
+
+def main():
+    """Main Program"""
+    start_time = time.clock()
+    largest = 0
+    longest_sequence = 0
+    Cache = dict()
+    for n in xrange(2, 1000001):
+        Number = n
+        counter = 0
+        while Number > 1:
+            try:
+                counter += Cache[Number] - 1
+                break
+            except KeyError:
+                pass
+            
+            if Number % 2 == 0:
+                Number = Number / 2
+            else:
+                Number = Number * 3 + 1
             counter += 1
-        else:
-            n = n*3 + 1
-            counter += 1
 
-    if counter > largest:
-        largest = counter
-print "largest = ", largest
-#print "starting number = ", number
+        counter += 1
+        Cache[n] = counter
 
+        if counter > longest_sequence:
+            longest_sequence = counter
+            largest = n
 
-
-
-
+    print "Answer = %s" % largest
+    print "longest sequence = %s" % longest_sequence
+    
+    run_time = time.clock() - start_time
+    print "Run time = ", run_time
         
-
-
+if __name__ == '__main__':
+    main()
