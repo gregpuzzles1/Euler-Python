91332d53f4253c9268e71e07407f7c164cbbbc9d
Greg Christian <glchristian@comcast.net>
2012-07-10 19:07:49 -0600

    problem 19 add

--- problem_19.py
+++ problem_19.py
@@ -1,4 +1,8 @@
+# Python version = 2.7.2
+# Platform = win32
+
 def find_days(month, yr):
+    """Finds the number of days the month has"""
     if (month == 1) or (month == 3) or (month == 5) or (month == 7) \
        or (month == 8) or (month == 10) or (month == 12):
         d = 31
@@ -7,9 +11,9 @@ def find_days(month, yr):
         d = 30
         return d
     elif (month == 2):
-        if yr%4 == 0:
-            if yr%100 == 0:
-                if yr%400 == 0:
+        if yr % 4 == 0:
+            if yr % 100 == 0:
+                if yr % 400 == 0:
                     d = 29
                     return d
                 else:
@@ -23,23 +27,32 @@ def find_days(month, yr):
         
     else:
         print "There has been an error"
+        
+L = {1:'Sun', 2:'Mon', 3:'Tue', 4:'Wed', 5:'Thurs', 6:'Fri', 7:'Sat'}
 
 def main():
-    for yr in range(1900,2001):
-    # Year
-        print "\n&&&&&&&&&&&&&&&&&&&&&&&year = ", yr
+    """Main Program"""
+    counter = 1 # Start at Monday, 1st, 1900
+    tally = 0
+    for yr in range(1900, 2001):
+    # Year  
+        print "\nYear = ", yr
         
-        for month in range(1,13):
+        for month in range(1, 13):
         # Month
-            print "\nmonth= ", month
             y = find_days(month, yr)
-            print "MONTH_DAYS = ", y
-            for days in range(1,(y+1)):
-            # Days
-                #print "day= ", days,
-                pass
-                         
+            
+            for month_days in range(1, (y + 1)):
+            # Number of month days
+                counter = counter + 1        
+                if (month_days == 1) and L[counter] == "Sun":
+                    print "month_days = %s L[counter]= %s " \
+                          % (month_days, L[counter])
+                    tally = tally + 1
+                if counter >= 7: # Days of the week
+                    counter = 0
+    print "\nTally = ", (tally - 2)
+    # subtract 2 for the year 1900                
 
 if __name__ == '__main__':
     main()
-        
