3ff121097721da0c18e6e6c919267d9fec72ea4b
Greg Christian <glchristian@comcast.net>
2012-07-13 15:18:13 -0600

    commit of problem 20

--- problem_20.py
+++ problem_20.py
@@ -1,8 +1,17 @@
-sumu = 1
-for i in range(1, 101):
-    sumu = sumu * i
-x = str(sumu)
-temp = 0
-for i in x:
-    temp = temp + int(i)
-print temp
+# Python version = 2.7.2
+# Platform = win32
+
+from math import factorial
+
+def main():
+    """Main Program"""
+    n = 100
+    Sum = 0
+    result = factorial(n)
+    string_result = str(result)
+    for number in string_result:
+        Sum += int(number)
+    print "Sum = %d" % Sum        
+    
+if __name__ == '__main__':
+    main()
